1a0322e43516f6adf4c71a721d23737c49f6776b
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-09-23 22:06:22 +0200

    GET: Leggo il solo nome con la funzione di get; SET: Setto nuovi parametri, atraverso la funzione di set

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 
 <?php
 
+// TRAIT da includere nella generale Persona
+trait FullName{
+
+  public $fullname_str;
+  public function fullName(){
+
+    return $fullname_str = "Nome: " . $this -> nome . ", Cognome: " . $this -> cognome;
+  }
+}
+
+
 // Creo la classe generale Persona
 class  Persona{
 
-  public $nome;
-  public $cogmome;
-  public $indirizzo;
+  use FullName;
+
+  protected $nome;
+  protected $cognome;
+  protected $indirizzo;
 
   public function __construct($nome, $cognome, $indirizzo){
 
     $this -> nome = $nome;
     $this -> cognome = $cognome;
     $this -> indirizzo = $indirizzo;
+  }
+
+  public function getNome(){
+
+    return "Nome: " . $this -> nome ;
+  }
+
+  public function setNome($nome){
+
+    return $this -> nome = $nome;
+  }
+  public function setCognome($cognome){
+
+    return $this -> cognome = $cognome;
+  }
+  public function setIndirizzo($indirizzo){
+
+    return $this -> indirizzo = $indirizzo;
   }
 
   public function toString(){
@@ -59,14 +90,21 @@ class Professore extends Persona{
   }
 }
 
-$persona1 = new Persona("Adriano", "De filippis", "Via Giovanni Falcone, 60");
-echo "<h1>ECHO DELLA CLASSE PERSONA<h1>" . $persona1 -> toString() . "<br>";
+// $persona1 = new Persona("Adriano", "De filippis", "Via Giovanni Falcone, 60");
+// echo "<h1>ECHO DELLA CLASSE PERSONA<h1>" . $persona1 -> toString() . "<br>";
 
-// Studente1 e prof1 ereditano la classe Persona
-$studente1 = new Studente("Adriano", "De filippis", "Via Giovanni Falcone, 60", "Informatica", "SI");
-echo "<h1>ECHO DELLA CLASSE STUDENTE<h1>" .  $studente1 -> toString() . "<br>";
+$persona1 = new Persona("Antonio", "orlando", "via Giovanni Falcone, 60");
+// LOG DI PERSONA 1
+echo $persona1 -> toString();
+// GET: Leggo il solo nome con la funzione di get
+echo "funzione Get per leggere il solo nome: " . $persona1 -> getNome();
+// SET: Setto nuovi parametri, atraverso la funzione di set
+$persona1 -> setNome("Giacomo");
+$persona1 -> setCognome("Gianrusso");
+$persona1 -> setIndirizzo("Via Alessandro");
+// LOG DI PERSONA1 dopo modifiche con funzione set
+echo $persona1 -> toString();
 
-$prof1 = new Professore("Paolo", "Orlandi", "via DAnte Alighieri, 20", "Programmazione REti aziendali" ,"40€/h");
-echo "<h1>ECHO DELLA CLASSE PROFESSORE<h1>" .  $prof1 -> toString();
-//
+
+
 ?>
